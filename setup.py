e2bdcafb122b6b77a2fe2283d02d49ff98c1cc25
Tim Speelman <timspeelman@live.nl>
2020-06-29 19:20:59 +0200

    Add setup and requirements

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='OpenWalletTS1',
+    version='0.1dev',
+    packages=find_packages(),
+    install_requires=[
+        "pyipv8"
+    ],
+    license='GNU Lesser General Public License'
+)
