bd486825c13bc8cfae5d26a696a25598a21a7f65
Tim Speelman <timspeelman@live.nl>
2020-07-01 12:10:08 +0200

    Fix path to static

--- ow_android/hello_endpoint.py
+++ ow_android/hello_endpoint.py
@@ -1,6 +1,7 @@
 from ipv8.REST.base_endpoint import BaseEndpoint, HTTP_BAD_REQUEST, HTTP_NOT_FOUND, Response
 from aiohttp import web
 from base64 import b64encode
+import os
 
 class HelloEndpoint(BaseEndpoint):
     """
@@ -11,7 +12,9 @@ class HelloEndpoint(BaseEndpoint):
         super(HelloEndpoint, self).__init__()
 
     def setup_routes(self):
-        path_to_static = "ow_android/static"
+        path_to_root = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__))))
+        path_to_static = os.path.join(path_to_root, 'static')
+    
         self.app.add_routes([web.static('/', path_to_static, show_index=True)])
 
     def initialize(self, session):
