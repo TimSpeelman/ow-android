3a7944ba9ad1e9e60b28b6c4352421c7c28115d8
Tim Speelman <timspeelman@live.nl>
2020-07-08 16:09:36 +0200

    Fix

--- ow_service.py
+++ ow_service.py
@@ -130,7 +130,7 @@ class OpenWalletService(object):
         priv = os.getenv("ANDROID_PRIVATE", os.getcwd())
         guidir = os.path.join(priv, "gui")
 
-        print("Static GUI files served from " guidir)
+        print("Static GUI files served from " + guidir)
         
         # Start its API
         api = RESTManager(ipv8)
